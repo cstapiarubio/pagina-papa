5f02f53198a70f08534cc3b7829ee97077714cb9
cstapiarubio <cstapiarubio@gmail.com>
2017-05-13 00:12:04 -0300

    esructura slider terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
-function Trabajos(foto1, foto2, foto3, parrafo){
+function Trabajos(foto1, foto2, foto3, foto4, foto5, foto6, parrafo){
 	this.foto1=foto1;
 	this.foto2=foto2;
 	this.foto3=foto3;
+	this.foto4=foto4;
+	this.foto5=foto5;
+	this.foto6=foto6;
     this.parrafo=parrafo;
 }
 var trabajo1=new Trabajos();
