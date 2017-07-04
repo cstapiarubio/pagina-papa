f896663b8edd3d3c27a30dcf86e2dfc61e516bda
cstapiarubio <cstapiarubio@gmail.com>
2017-07-03 20:08:42 -0400

    arregle el carousel, y detalles de alineacion, faltan pequeños detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,1 @@
-function Trabajos(foto1, foto2, foto3, foto4, foto5, foto6, parrafo){
-	this.foto1=foto1;
-	this.foto2=foto2;
-	this.foto3=foto3;
-	this.foto4=foto4;
-	this.foto5=foto5;
-	this.foto6=foto6;
-    this.parrafo=parrafo;
-}
-var trabajo1=new Trabajos();
+$('body').scrollspy({target: ".navbar"})
